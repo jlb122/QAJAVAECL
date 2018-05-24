a725328d17534021f733c3693a4dfcdb962ed1ed
James Bridger <jamesbridger@live.com>
2018-05-24 12:37:39 +0100

    Adding exercises performed in class on Thursday morning. Contains arrays and collections

---
+++ Labs/workspace/DG00_HelloWorld/src/com/qa/Demo1_HelloWorld.java
@@ -0,0 +1,88 @@
+package com.qa;
+
+public class Demo1_HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * Multi Line Comment
+		 */
+		
+//		System.out.println("Hello, World");
+		
+		// Number Types - Primitives
+		int x = Integer.MAX_VALUE;
+		double d = Double.MAX_VALUE;
+		byte b = Byte.MAX_VALUE;
+		short s = Short.MAX_VALUE;
+		float f = Float.MAX_VALUE;
+		
+		// Other Primitives...
+		char a = 'a';
+		char c = '\u0012';
+		//System.out.println(c);
+		
+		boolean over40, male; // OK but not good practice as we can't easily see all declaration
+							  // Good practice to initialise on declaration
+		over40 = true;
+		male = true;
+		//System.out.println(over40);
+		//System.out.println(male);
+		
+		// Behaviour
+//		System.out.println(x);
+//		x += 1;					// same as x = x + 1;
+//		System.out.println(x);
+		
+		// Incrementers and Decrementers
+		int number = 0;
+		
+//		System.out.println(number);		// 0
+//		System.out.println(number++);	// 0
+//		System.out.println(number);		// 1
+//		System.out.println(++number);	// 2
+//		System.out.println(number);		// 2
+		
+		// Special Case...
+//		String myString = "Hello, World!";
+//		System.out.println(myString);
+//		System.out.println(myString.length());
+//		String statement = "This is answer to the ultimate question!";
+//		System.out.println(statement.length());
+//		
+//		System.out.println(myString == "Hello World"); // Works but not good!
+//		System.out.println(myString.equals("Hello, World!"));
+//		
+//		System.out.println(myString.toString());
+//		System.out.println(myString.toString());
+		
+		// Arrays - generally of primitives or of objects
+		int[] array1 = new int[7];		// Declares an empty 7 element array type of int
+		int[] array2 = {10, 20, 30, 40, 50, 60, 70};  // Declare and initialise elements
+		String[] array3 = {"Rod", "Jane", "Freddie"}; // Declare and initialise elements
+		// SIZES of array 1-3 are now fixed
+		
+		int numFromArray = array1[3];
+		int numFromArray2 = array2[5];
+		String stringFromArray3 = array3[2];
+		
+		System.out.println(numFromArray);		// null? -> 00000000000
+		System.out.println(numFromArray2);		// 60
+		System.out.println(stringFromArray3);	// Freddie
+		
+//		System.out.println(array3[4]);  		// ArrayIndexOutOfBoundsException
+		
+//		array2[8] = 10;
+		array2[1] = (int) 10.56;
+		System.out.println(array2[1]);
+		double newDouble = (double) array2[1];
+		System.out.println(newDouble);
+		
+		for(int i = 0; i < array2.length; i++) {
+			System.out.println(array2[i]);
+			
+		}
+
+	}
+
+}
---
+++ Labs/workspace/DG07_Collections/src/demo2/maps/MapsMain.java
@@ -0,0 +1,56 @@
+package demo2.maps;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class MapsMain {
+
+	public static void main(String[] args) {
+		
+		// Demo 7.6 - Maps
+		// Allow mapping of unique keys to values
+		
+		Map<String, String> map1 = new HashMap<>();
+		
+		// Put items into map1
+		map1.put("Cuthbert", "14");
+		map1.put("Pugh", "8");
+		map1.put("Pugh", "31");
+		map1.put("Barney McGrew", "12");
+		
+		System.out.println("Hash Map Elements");
+		System.out.println("\t" + map1);
+		
+		// Demo 7.7 - TreeMaps
+		// Provides efficient means of storing key/value pairs in stored order
+		// Allows rapid retrieval
+		TreeMap<String, Double> treeMap = new TreeMap<>();
+		
+		treeMap.put("Pugh", new Double(4321.12));
+		treeMap.put("Pugh", new Double(123.45));
+		treeMap.put("Barney McGrew", new Double(7654.21));
+		treeMap.put("Cuthbert", new Double(456.123));
+		
+		System.out.println("Tree Map Elements");
+		System.out.println("\t" + treeMap);
+		
+		// Demo 7.8 - Linked Hash Map
+		// Extends HashMap but it maintains linked list of entries in the order of 
+		
+		Map<String, String> map2 = new LinkedHashMap<>();
+		
+		// Put items into map2
+		map2.put("Cuthbert", "14");
+		map2.put("Pugh", "8");
+		map2.put("Pugh", "31");
+		map2.put("Barney McGrew", "12");
+		
+		System.out.println("Linked Hash Map Elements");
+		System.out.println("\t" + map2);
+		
+
+	}
+
+}
